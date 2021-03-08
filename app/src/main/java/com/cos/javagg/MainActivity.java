5c988ea3fef3697eca525272cee02560d710fa66
otk2625 <xorud8531@gmail.com>
2021-03-08 17:18:15 +0900

    디자인 살짝 변경

--- app/src/main/java/com/cos/javagg/MainActivity.java
+++ app/src/main/java/com/cos/javagg/MainActivity.java
@@ -1,14 +1,63 @@
 package com.cos.javagg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.cos.javagg.fragment.ChampionFragment;
+import com.cos.javagg.fragment.CommunityFragment;
+import com.cos.javagg.fragment.LoginFragment;
+import com.cos.javagg.fragment.RankingFragment;
+import com.cos.javagg.fragment.SearchFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Context mContext = MainActivity.this;
+    private BottomNavigationView bottomNavigationView;
+
+    public Integer num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //최초 화면
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SearchFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId())  {
+                case  R.id.bottom_search:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.bottom_community :
+                    selectedFragment = new CommunityFragment();
+                    break;
+                case R.id.bottom_champion :
+                    selectedFragment = new ChampionFragment();
+                    break;
+                case R.id.bottom_ranking :
+                    selectedFragment = new RankingFragment();
+                    break;
+                case R.id.bottom_login :
+                    selectedFragment = new LoginFragment();
+                    break;
+
+            }
+            //fragmanet 바꿔치기
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        });
+
     }
+
 }
