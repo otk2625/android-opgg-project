6b3eb2acbe981d37744bd5280a04509f453b474e
otk2625 <xorud8531@gmail.com>
2021-03-11 17:08:45 +0900

    커뮤니티 postdetail, 댓글 디자인 구현

--- app/src/main/java/com/cos/javagg/fragment/SearchFragment.java
+++ app/src/main/java/com/cos/javagg/fragment/SearchFragment.java
@@ -3,14 +3,13 @@ package com.cos.javagg.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,45 +20,45 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
-import com.cos.javagg.MainActivity;
 import com.cos.javagg.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class SearchFragment extends Fragment {
-    private Toolbar toolbar;
-    private Vibrator vibrator;
-    private NavigationView navigationView;
+
     private DrawerLayout mDrawerLayout;
-    private Button btn1;
-    private MainActivity at;
+    private ImageButton draw;
+    private Vibrator vibrator;
+    private ImageButton btn1;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        at = (MainActivity)container.getContext();
         View view = inflater.inflate(R.layout.fragment_search,container,false);
 
+        mDrawerLayout=view.findViewById(R.id.drawerLayout);
+        draw = view.findViewById(R.id.draw);
+        vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        btn1=view.findViewById(R.id.img_button);
 
-//       툴바 셋팅
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        MainActivity activity = (MainActivity) getActivity();
-        activity.setSupportActionBar(toolbar);
-        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
-        actionBar.setDisplayShowCustomEnabled(false);
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayHomeAsUpEnabled(true);          // 바_게임
-        actionBar.setHomeAsUpIndicator(R.mipmap.game);
-
-        View viewToolbar = getActivity().getLayoutInflater().inflate(R.layout.toolbar_search, null);
-        setHasOptionsMenu(true);
-
+        drawL();    // 드로우레이아웃
+        lolclick(); // 롤 이미지클릭
 
         return view;
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) { // res/menu 에서 친구 탭에서 작동 할 menu를 가져온다.
-        inflater.inflate(R.menu.searchnavi_menu, menu);
-        super.onCreateOptionsMenu(menu, inflater);
+    public void lolclick() {
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vibrator.vibrate(100);
+            }
+        });
     }
 
+    public void drawL(){
+        draw.setOnClickListener(v -> {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        });
+    }
 }
